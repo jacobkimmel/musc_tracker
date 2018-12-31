55b6297bac2ddeb3d2d31232a04b15bfe1dfdd22
Jacob Kimmel <jacobkimmel@gmail.com>
2018-12-31 14:22:33 -0800

    alter CLI args

--- track_well.py
+++ track_well.py
@@ -1,7 +1,6 @@
 '''Track cells in a given well'''
 
 import numpy as np
-import argparse
 import os
 import glob
 from mask_parser import MaskParser
@@ -9,27 +8,6 @@ from labeled_movie_maker import TrackingMovie, ExportTrackingMovie
 from bipartite_tracker import BipartiteTracker
 from tracking_fnx import track_fov
 import matplotlib.pyplot as plt
-
-parser = argparse.ArgumentParser(description='Track cells in a target well and generate tracking videos.')
-parser.add_argument('well_dir', type=str, help='path to well directory')
-parser.add_argument('--img_regex', type=str, default='*c1.tif',  help='pattern to match image files')
-parser.add_argument('--mask_regex', type=str, default='*csn.png', help='pattern to match mask files')
-parser.add_argument('--max_xy', type=int, default=30, help='maximum number of xy positions')
-parser.add_argument('--time_regex', type=str, default='%03d', help='format string for time index')
-parser.add_argument('--max_dist', type=int, default=100, help='maximum distance for frame linking')
-parser.add_argument('--gap_frames', type=int, default=15, help='maximum tracking gap')
-parser.add_argument('--app_model', action='store_true', help='use CNN appearance model')
-parser.add_argument('--min_t', type=int, default=0, help='start time for tracking')
-parser.add_argument('--max_t', type=int, default=None, help='end time for tracking')
-parser.add_argument('--singlethread', action='store_true', help='run tracking on a single thread. can avoid issues with matplotlib IO bottlenecking.')
-parser.add_argument('--novideo', action='store_true', help='do not export tracking videos (single threaded bottleneck)')
-parser.add_argument('--prefix', type=str, default='', 
-                    help='prefix for outputs.')
-
-args = parser.parse_args()
-
-xys = [str(x).zfill(int(np.log10(args.max_xy)+1)) for x in range(1,args.max_xy + 1)]
-
 
 def track(i, xys, args):
     '''Track a single `xy` in `xys`'''
@@ -88,11 +66,11 @@ def make_movie(freq_dir, mp, xc, yc, img_regex, out_lab, min_t, max_t):
     plt.close()
 
     print('Saved ', movie_path)
-    
+
 def make_ffmpeg_movie(freq_dir, mp, xc, yc, img_regex, out_lab, min_t, max_t):
-    
+
     out_dir = freq_dir
-    
+
     imgp = MaskParser(freq_dir,
                 regex=img_regex, no_parsing=True)
     movie_path = os.path.join(out_dir, out_lab + 'movie.avi')
@@ -104,38 +82,74 @@ def make_ffmpeg_movie(freq_dir, mp, xc, yc, img_regex, out_lab, min_t, max_t):
     etm.save_frames()
     #etm.call_ffmpeg()
 
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(
+        description='Track cells in a target well and generate tracking videos.')
+    parser.add_argument('well_dir', type=str,
+        help='path to well directory')
+    parser.add_argument('--img_regex', type=str, default='*c1.tif',
+        help='pattern to match image files')
+    parser.add_argument('--mask_regex', type=str, default='*.png',
+        help='pattern to match mask files')
+    parser.add_argument('--max_xy', type=int, default=30,
+        help='maximum number of xy positions')
+    parser.add_argument('--time_regex', type=str, default='%03d',
+        help='format string for time index')
+    parser.add_argument('--max_dist', type=int, default=100,
+        help='maximum distance for frame linking')
+    parser.add_argument('--gap_frames', type=int, default=15,
+        help='maximum tracking gap')
+    parser.add_argument('--app_model', action='store_true',
+        help='use CNN appearance model')
+    parser.add_argument('--min_t', type=int, default=0,
+        help='start time for tracking')
+    parser.add_argument('--max_t', type=int, default=None,
+        help='end time for tracking')
+    parser.add_argument('--singlethread', action='store_true',
+        help='run tracking on a single thread. can avoid issues with matplotlib IO bottlenecking.')
+    parser.add_argument('--novideo', action='store_true',
+        help='do not export tracking videos (single threaded bottleneck)')
+    parser.add_argument('--one_indexed', action='store_true',
+        help='denotes that xy positions are one indexed, rather than zero indexed.')
+    parser.add_argument('--prefix', type=str, default='',
+                        help='prefix for outputs.')
 
-from functools import partial
-import multiprocessing
+    args = parser.parse_args()
 
-part_track = partial(track, xys=xys, args=args)
+    xys = [str(x).zfill(int(np.log10(args.max_xy)+1)) for x in range(int(args.one_indexed), args.max_xy + int(args.one_indexed))]
 
-if args.app_model or args.singlethread:
-    for xy in range(len(xys)):
-        track(xy, xys, args)
-else:
-    p = multiprocessing.Pool()
+    from functools import partial
+    import multiprocessing
 
-    res = p.map(part_track, range(len(xys)))
+    part_track = partial(track, xys=xys, args=args)
 
-    p.close()
-
-
-def movie_wrapper(i, res):
-    params = res[i]
-    make_ffmpeg_movie(*params)
-    return 
-
-part_movie = partial(movie_wrapper, res=res)
-
-if not args.novideo:
-    
-    if args.singlethread:
-        for i in range(len(res)):
-            params = res[i]
-            make_ffmpeg_movie(*params)
-            
+    if args.app_model or args.singlethread:
+        for xy in range(len(xys)):
+            track(xy, xys, args)
     else:
         p = multiprocessing.Pool()
-        movie_out = p.map(part_movie, range(len(res)))
+
+        res = p.map(part_track, range(len(xys)))
+
         p.close()
+
+
+    def movie_wrapper(i, res):
+        params = res[i]
+        make_ffmpeg_movie(*params)
+        return
+
+    part_movie = partial(movie_wrapper, res=res)
+
+    if not args.novideo:
+
+        if args.singlethread:
+            for i in range(len(res)):
+                params = res[i]
+                make_ffmpeg_movie(*params)
+
+        else:
+            p = multiprocessing.Pool()
+            movie_out = p.map(part_movie, range(len(res)))
+            p.close()
