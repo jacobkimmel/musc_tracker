45f6708e1617137f672141a97a598db614e8e6e1
Jacob Kimmel <jacobkimmel@gmail.com>
2019-01-07 00:25:22 -0800

    interp masks updates

--- interpolate_masks_well.py
+++ interpolate_masks_well.py
@@ -11,14 +11,14 @@ parser.add_argument('--mask_glob', type=str, default='*densenet.png', help='patt
 parser.add_argument('--gap_size', type=int, default=5, help='gap sizes to interpolate')
 parser.add_argument('--output_suffix', type=str, default='_temporal_smoothing', help='suffix for output files')
 parser.add_argument('--max_xy', type=int, default=30, help='maximum number of xy positions')
-parser.add_argument('--singlethread', action='store_true', help='perform single threaded operations')
+parser.add_argument('--n_jobs', type=int, default=1, help='number of threads to use.')
 args = parser.parse_args()
 
 
 def mask_interp(xy, mask_dir, mask_glob, gap_size, output_suffix):
-    
+
     print('INTERPOLATING XY ', str(xy).zfill(2))
-    MI = MaskInterpolator(mask_dir=mask_dir, 
+    MI = MaskInterpolator(mask_dir=mask_dir,
                           mask_glob='*xy' + str(xy).zfill(2) + mask_glob,
                           gap_size=gap_size,
                           output_suffix=output_suffix,
@@ -32,13 +32,13 @@ part_mask_interp = partial(mask_interp,
                           mask_glob=args.mask_glob,
                           gap_size=args.gap_size,
                           output_suffix=args.output_suffix)
-   
-    
 
-if args.singlethread:
-    for xy in range(1, args.max_xy+1):
+
+
+if args.n_jobs == 1:
+    for xy in range(0, args.max_xy):
         print('INTERPOLATING XY ', str(xy).zfill(2))
-        MI = MaskInterpolator(mask_dir=args.mask_dir, 
+        MI = MaskInterpolator(mask_dir=args.mask_dir,
                               mask_glob='*xy' + str(xy).zfill(2) + args.mask_glob,
                               gap_size=args.gap_size,
                               output_suffix=args.output_suffix,
@@ -46,11 +46,8 @@ if args.singlethread:
 
         MI.fill_gaps()
         MI.export_filled_masks()
-        
+
 else:
-    p = Pool()
-    res = p.map(part_mask_interp, range(1, args.max_xy + 1))
+    p = Pool(args.n_jobs)
+    res = p.map(part_mask_interp, range(0, args.max_xy))
     p.close()
-    
-    
-    
